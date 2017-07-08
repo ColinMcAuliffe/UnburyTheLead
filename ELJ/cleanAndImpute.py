4090db887d76d104daf2dd76a81417fdc7a9c7f2
colinmcauliffe2176 <therealcolinmcauliffe@gmail.com>
2017-07-07 20:07:42 -0400

    Imputation stuff and rewrote abstract

--- ELJ/cleanAndImpute.py
+++ ELJ/cleanAndImpute.py
@@ -9,7 +9,7 @@ abbr2name = us.states.mapping('abbr', 'name')
 name2abbr = us.states.mapping('name', 'abbr')
 
 dataDir       = "data"
-
+np.seterr(all='raise')
 #load in original csv and clean it up a bit
 congress = pd.read_csv(os.path.join(dataDir,"US_House_elections_1898_2016.csv"))
 congress['RepVotes'] = congress['RepVotes'].str.replace(',','') 
@@ -40,11 +40,11 @@ def year2Cycle(year):
         return 2010
 
 #Data by district and cycle
-dataDC     = [['State','cycle','AreaNumber','avgDemVote','avgRepVote']]
+dataDC     = [['State','cycle','AreaNumber','avgDemFrac','avgRepFrac']]
 #Data by state and cycle
-dataSC     = [['State','cycle','avgDemVoteInMostPartisanDem','avgRepVoteInMostPartisanDem','avgDemVoteInMostPartisanRep','avgRepVoteInMostPartisanRep']]
+dataSC     = [['State','cycle','avgDemFracInMostPartisanDem','avgDemFracInMostPartisanRep']]
 #Data by state and year
-dataSY     = [['State','year','contestedFrac']]
+dataSY     = [['State','year','contestedFrac','avgTurnout']]
 
 for state in states:
     abbr = state.abbr
@@ -56,29 +56,28 @@ for state in states:
         #Find the average vote percentages in the most partisan districts
         demInMostPartisanDem = []
         demInMostPartisanRep = []
-        repInMostPartisanDem = []
-        repInMostPartisanRep = []
         for year in cyc:
             dfYear = dfCycle[dfCycle["raceYear"] == year]
             #Filter uncontested
             dfYear = dfYear[dfYear["Dem Vote %"] != 0.0]
             dfYear = dfYear[dfYear["Dem Vote %"] != 1.0]
-            dataSY.append([abbr,year,float(len(dfYear))/float(numDistricts)])
+            #get average turnout
+            if len(dfYear) > 0:
+                avgTurnout = np.mean(dfYear["DemVotes"].values + dfYear["RepVotes"].values)
+            else:
+                avgTurnout = 0.
+            dataSY.append([abbr,year,float(len(dfYear))/float(numDistricts),avgTurnout])
 
             if len(dfYear) > 0:
                 mostPartisanDemIdx = dfYear["Dem Vote %"].argmax() 
                 mostPartisanRepIdx = dfYear["Dem Vote %"].argmin() 
 
-                demInMostPartisanDem.append(dfYear["DemVotes"].loc[mostPartisanDemIdx])
-                demInMostPartisanRep.append(dfYear["DemVotes"].loc[mostPartisanRepIdx])
-                repInMostPartisanDem.append(dfYear["RepVotes"].loc[mostPartisanDemIdx])
-                repInMostPartisanRep.append(dfYear["RepVotes"].loc[mostPartisanRepIdx])
+                demInMostPartisanDem.append(dfYear["Dem Vote %"].loc[mostPartisanDemIdx])
+                demInMostPartisanRep.append(dfYear["Dem Vote %"].loc[mostPartisanRepIdx])
         
-        avgDemVoteInMostPartisanDem = np.mean(demInMostPartisanDem)
-        avgRepVoteInMostPartisanDem = np.mean(repInMostPartisanDem)
-        avgDemVoteInMostPartisanRep = np.mean(demInMostPartisanRep)
-        avgRepVoteInMostPartisanRep = np.mean(repInMostPartisanRep)
-        dataSC.append([abbr,state.fips,cnm,avgDemVoteInMostPartisanDem,avgRepVoteInMostPartisanDem,avgDemVoteInMostPartisanRep,avgRepVoteInMostPartisanRep])
+        avgDemFracInMostPartisanDem = np.mean(demInMostPartisanDem)
+        avgDemFracInMostPartisanRep = np.mean(demInMostPartisanRep)
+        dataSC.append([abbr,cnm,avgDemFracInMostPartisanDem,avgDemFracInMostPartisanRep])
 
         #Find the average dem and rep vote percentages in each district over all contested election in a cycle
         for i in range(numDistricts):
@@ -89,18 +88,15 @@ for state in states:
             demVote = np.array(dfDistrict["DemVotes"].values[mask])
             repVote = np.array(dfDistrict["RepVotes"].values[mask])
             if len(demVote) > 0:
-                dmean = np.mean(demVote)
-                rmean = np.mean(repVote)
+                dmean = np.mean(votePct[mask])
             else:
                 if votePct[0] == 0.0:
-                    dmean = avgDemVoteInMostPartisanRep
-                    rmean = avgRepVoteInMostPartisanRep
+                    dmean = avgDemFracInMostPartisanRep
                 elif votePct[0] == 1.0:
-                    dmean = avgDemVoteInMostPartisanDem
-                    rmean = avgRepVoteInMostPartisanDem
+                    dmean = avgDemFracInMostPartisanDem
                 else:
                     dmean = 0.0
-                    rmean = 0.0
+            rmean = 1.-dmean
             dataDC.append([abbr,cnm,i+1,dmean,rmean])
             
 
@@ -124,6 +120,22 @@ new_header = data.iloc[0] #grab the first row for the header
 data = data[1:] #take the data less the header row
 data.columns = new_header #set the header row as the df header
 data.index = range(len(data))
+#manually fix turnout for states with no contested elections
+idx = data[data['avgTurnout']==0.].index.tolist()
+stCol = data.columns.get_loc('State')
+avCol = data.columns.get_loc('avgTurnout')
+for i in idx:
+    state   = data.iloc[i,stCol]
+    avgTurnout = 0.
+    count      = 0.
+    if state == data.iloc[i-1,stCol]:
+        avgTurnout += data.iloc[i-1,avCol]
+        count += 1.
+    if state == data.iloc[i+1,stCol]:
+        avgTurnout += data.iloc[i+1,avCol]
+        count += 1.
+    data.iloc[i,avCol] = avgTurnout/count
+
 fig = plt.figure()
 ax = fig.add_subplot(111)
 for state in states:
@@ -132,7 +144,7 @@ for state in states:
     dfState = data[data["State"] == abbr]
     ax.plot(dfState['contestedFrac'])
 fig.savefig("contested.png")
-data.to_csv(os.path.join(dataDir,"contestedRate.csv"))
+data.to_csv(os.path.join(dataDir,"avgTurnout.csv"))
 
 data = pd.DataFrame(dataDC)
 new_header = data.iloc[0] #grab the first row for the header
@@ -148,7 +160,8 @@ data.columns = new_header #set the header row as the df header
 data.index = range(len(data))
 data.to_csv(os.path.join(dataDir,"avgVotesInMostPartisanByStateAndCycle.csv"))
 
-imputeData = pd.read_csv(os.path.join(dataDir,"avgVotesByDistrictAndCycle.csv"))
+imputeData = pd.read_csv(os.path.join(dataDir,"avgTurnout.csv"))
+imputeData2 = pd.read_csv(os.path.join(dataDir,"avgVotesByDistrictAndCycle.csv"))
 imputedRep = []
 imputedDem = []
 for idx,row in congress.iterrows():
@@ -156,12 +169,24 @@ for idx,row in congress.iterrows():
         year = row["raceYear"]
         state = row["State"]
         dist = row["AreaNumber"]
+        dfYear = imputeData[imputeData["year"]==year]
+        dfState = dfYear[dfYear["State"]==name2abbr[state]]
+        avgTurnout = dfState["avgTurnout"].values[0]
+        #For 75-25 split method
+        #if row["Dem Vote %"] == 1.0:
+        #    imputedRep.append(avgTurnout*.25)
+        #    imputedDem.append(avgTurnout*.75)
+        #else:
+        #    imputedRep.append(avgTurnout*.75)
+        #    imputedDem.append(avgTurnout*.25)
+
+
         cycle = year2Cycle(year)
-        dfCycle = imputeData[imputeData["cycle"]==cycle]
+        dfCycle = imputeData2[imputeData2["cycle"]==cycle]
         dfState = dfCycle[dfCycle["State"]==name2abbr[state]]
         dfDist  = dfState[dfState["AreaNumber"]==dist]
-        imputedRep.append(dfDist["avgRepVote"].values[0])
-        imputedDem.append(dfDist["avgDemVote"].values[0])
+        imputedRep.append(avgTurnout*dfDist["avgRepFrac"].values[0])
+        imputedDem.append(avgTurnout*dfDist["avgDemFrac"].values[0])
     else:
         imputedRep.append(row["RepVotes"])
         imputedDem.append(row["DemVotes"])
